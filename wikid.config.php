a1379f5d8f922164977628ffdd2e52dadc86e97c
Maigre <thomas.bohl@gmail.com>
2012-06-18 14:01:46 +0200

    MGR starting with a clean CodeIgniter

---
+++ wikid.config.php
@@ -0,0 +1,30 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ *---------------------------------------------------------------
+ * WIKID Website Managment System Configuration
+ *---------------------------------------------------------------
+ */
+ 
+
+$wikid_config = array (
+	
+	//Website Name and Title
+	'WKD_NAME'		=>	'Dummy Wikid',
+	
+	//Website Short Code (no space / no special chars)
+	'WKD_CODE'		=>	'DmyWkd',
+	
+	//Base url to access the website
+	'WKD_BASE_URL'		=>	'http://localhost/Wikid/',
+	
+	//Database connection
+	'WKD_DB_DRIVER'		=>	'mysql',
+	'WKD_DB_HOST'		=>	'localhost',
+	'WKD_DB_BASE'		=>	'wikid',
+	'WKD_DB_USER'		=>	'wikid',
+	'WKD_DB_PASS'		=>	'wikid37',
+	
+	
+
+);
